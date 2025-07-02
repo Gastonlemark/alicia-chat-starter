3c52db806dd7abd059a893298fa4e08ed7b05cce
Gastonlemark <espaciopublicitario8@gmail.com>
2025-07-02 13:44:43 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Hola, soy Alicia</h1>
-      <p>Estoy lista para conversar contigo, Juan Pablo. 🩵</p>
-    </div>
-  );
-}
