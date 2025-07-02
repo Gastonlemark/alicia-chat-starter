a61c74b0268502f4a6672a9f3138783b8ed9198b
Gastonlemark <espaciopublicitario8@gmail.com>
2025-07-02 13:53:37 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hola, soy Alicia</h1>
+      <p>Estoy lista para conversar contigo, Juan Pablo. 🩵</p>
+    </div>
+  );
+}
